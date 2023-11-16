8fb767c3d7602fe595b66b17c9fd58f8d3b03f7a
kat <katbetancourt88@gmail.com>
2023-11-16 08:21:12 -0500

    Fiest commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import user from './profile.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
